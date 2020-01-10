eb797066ecb5d26b4e2ea8553c4ffe56d293be05
Sara Gorecki <sgorecki@gmail.com>
2020-01-10 17:51:16 -0500

    Print board of dead cells

--- board.py
+++ board.py
@@ -5,7 +5,14 @@ class Board:
     def __init__(self, width=25, height=25):
         self.width = width
         self.height = height
+        self.rendered_board_as_list = []
         self.board = self.create()
+
+    RENDERING_STRINGS = {
+        'dead': '.',
+        'alive': 'o',
+        'zombie': 'x',
+    }
 
     def create(self):
         current_row = 0
@@ -14,10 +21,26 @@ class Board:
         
         while current_row < self.height:
             board.append([])
+            self.rendered_board_as_list.append([])
             while current_col < self.width:
-                board[current_row].append(Cell('dead', current_row, current_col))
+                cell_state = 'dead'
+                board[current_row].append(Cell(cell_state, current_row, current_col))
+                self.rendered_board_as_list[current_row].append(self.RENDERING_STRINGS[cell_state])
                 current_col = current_col + 1
             current_row = current_row + 1
             current_col = 0
         
+        print(self.rendered_board_as_list)
         return board
+
+    def draw_board(self):
+        current_row = 0
+        first_and_last_rows = '_' * (self.width + 2) + '\n'
+        rendered_board = first_and_last_rows
+        while current_row < self.height:
+            rendered_board += '|' + ''.join(self.rendered_board_as_list[current_row]) + '|\n'
+            print (rendered_board)
+            current_row += 1
+        rendered_board += first_and_last_rows
+
+        print(rendered_board)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from board import Board
 def main():
     print('Welcome to the Game of Life')
     board = Board(5, 5)
-    print(board.board)
+    board.draw_board()
 
 if __name__ == "__main__":
     main()
