edce8c044e61793955d2a2354dbbb4fed6be0f8f
Sara Gorecki <sgorecki@gmail.com>
2020-01-14 17:07:28 -0500

    adjust so cell looks at its own neighbors

--- cell.py
+++ cell.py
@@ -5,47 +5,45 @@ class Cell:
     def __init__(self, board, state, row, col):
         self.board = board
         self.state = state
-        self.previous_state = None
+        self.next_state = None
         self.row = row
         self.col = col
         self.count_alive_neighbors = 0
         self.count_zombie_neighbors = 0
 
-    def adjust_neighbor_population_count(self, neighboring_cell):
-        if self.previous_state is 'alive':
-            neighboring_cell.count_alive_neighbors =- 1
-        if self.previous_state is 'zombie':
-            neighboring_cell.count_zombie_neighbors =- 1
-        if self.state is 'alive':
-            neighboring_cell.count_alive_neighbors =+ 1
-        if self.state is 'zombie':
-            neighboring_cell.count_zombie_neighbors =+ 1
+    def get_neighbor_state(self, neighboring_cell):
+        if neighboring_cell.state is 'alive':
+            self.count_alive_neighbors =+ 1
+        if neighboring_cell.state is 'zombie':
+            self.count_zombie_neighbors =+ 1
 
-    def update_neighboring_cells(self):
+    def check_neighboring_cells(self):
         board = self.board.board
-        board[self.row-1][self.col-1]
-        board[self.row-1][self.col]
-        board[self.row-1][self.col+1]
-        board[self.row][self.col-1]
-        board[self.row][self.col+1]
-        board[self.row+1][self.col-1]
-        board[self.row+1][self.col]
-        board[self.row+1][self.col+1]
+        self.get_neighbor_state(board[self.row-1][self.col-1])
+        self.get_neighbor_state(board[self.row-1][self.col])
+        self.get_neighbor_state(board[self.row-1][self.col+1])
+        self.get_neighbor_state(board[self.row][self.col-1])
+        self.get_neighbor_state(board[self.row][self.col+1])
+        self.get_neighbor_state(board[self.row+1][self.col-1])
+        self.get_neighbor_state(board[self.row+1][self.col])
+        self.get_neighbor_state(board[self.row+1][self.col+1])
 
     def update_state(self):
-        self.previous_state = self.state
-        if self.previous_state == 'alive':
-            if self.count_live_neighbors < 2 or self.count_live_neighbors > 3:
-                self.state = 'dead'
+        if self.state == 'alive':
+            if self.count_alive_neighbors < 2 or self.count_alive_neighbors > 3:
+                self.next_state = 'dead'
                 # TODO: if no zombies on the board, 5% chance of becoming a zombie
             if self.count_zombie_neighbors > 1:
-                self.state = 'zombie'
-        if self.previous_state == 'dead':
-            if self.count_live_neighbors is 3:
-                self.state = 'alive'
-        if self.previous_state == 'zombie':
-            if self.count_live_neighbors > 2:
-                self.state = 'dead'
+                self.next_state = 'zombie'
+        elif self.state == 'dead':
+            if self.count_alive_neighbors is 3:
+                self.next_state = 'alive'
+        elif self.state == 'zombie':
+            if self.count_alive_neighbors > 2:
+                self.next_state = 'dead'
+        else:
+            self.next_state = self.state
 
-        if self.previous_state is not self.state:
-            self.update_neighboring_cells()
+    def take_turn(self):
+        self.check_neighboring_cells()
+        self.update_state()
