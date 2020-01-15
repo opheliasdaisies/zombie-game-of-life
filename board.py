239121354a45d396d936c451972478b81c7b186f
Sara Gorecki <sgorecki@gmail.com>
2020-01-15 14:55:42 -0500

    Use grid directly to draw game board

--- board.py
+++ board.py
@@ -5,7 +5,6 @@ class Board:
     def __init__(self, width=25, height=25):
         self.width = width
         self.height = height
-        self.rendered_board_as_list = []
         self.grid = self.create()
 
     RENDERING_STRINGS = {
@@ -21,11 +20,9 @@ class Board:
         
         while current_row < self.height:
             board.append([])
-            self.rendered_board_as_list.append([])
             while current_col < self.width:
                 cell_state = 'dead'
                 board[current_row].append(Cell(self, cell_state, current_row, current_col))
-                self.rendered_board_as_list[current_row].append(self.RENDERING_STRINGS[cell_state])
                 current_col = current_col + 1
             current_row = current_row + 1
             current_col = 0
@@ -33,10 +30,15 @@ class Board:
         return board
 
     def draw_board(self):
-        first_and_last_rows = '_' * (self.width + 2) + '\n'
-        rendered_board = first_and_last_rows
-        for row in self.rendered_board_as_list:
-            rendered_board += '|' + ''.join(row) + '|\n'
-        rendered_board += first_and_last_rows
+        border_row = '_' * (self.width + 2) + '\n'
+
+        rendered_board = border_row
+
+        for row in self.grid:
+            rendered_board += '|'
+            for cell in row:
+                rendered_board += self.RENDERING_STRINGS[cell.state]
+            rendered_board += '|\n'
+        rendered_board += border_row
 
         print(rendered_board)
