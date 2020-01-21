136d596dafc0967e7436fef92e7c9338f608911c
Sara Gorecki <sgorecki@gmail.com>
2020-01-20 21:50:46 -0500

    refactor drawing board and cell selection

--- board.py
+++ board.py
@@ -1,4 +1,3 @@
-import os
 import tty
 import sys
 from cell import Cell
@@ -9,6 +8,8 @@ class Board:
         self.width = width
         self.height = height
         self.grid = self.create()
+        self.cursor_row = 0
+        self.cursor_col = 0
 
     BLACK_BACKGROUND = '\u001b[40m'
     WHITE_BACKGROUND = '\u001b[47m'
@@ -58,23 +59,86 @@ class Board:
 
     def draw_board(self):
         borders = self.get_borders()
-        os.system('cls' if os.name == 'nt' else 'clear')
-        rendered_board = f'{borders["top"]}{self.BLACK_BACKGROUND}'
+        print(chr(27) + '[2J' + chr(27) + '[0;0H') # clears terminal
+        rendered_board = f'\n{self.GREEN_TEXT}Let the Game Begin!\n(press CTRL-C to exit)\n\n{borders["top"]}{self.BLACK_BACKGROUND}'
 
         for row in self.grid:
             rendered_board += borders['left']
             for cell in row:
                 rendered_state = cell.next_state or cell.state
                 rendered_board += self.RENDERING_STRINGS[rendered_state]
-            rendered_board += f'{borders["right"]}\n\r'
+            rendered_board += f'{borders["right"]}\n'
 
         print(rendered_board + self.TEXT_RESET + borders['bottom'])
+
+    def process_keyboard_input(self):
+        char = ord(sys.stdin.read(1)) # listen for arrow keys and get char code
+        if char == 27: # Arrow keys process as a sequence of three characters
+            next1, next2 = ord(sys.stdin.read(1)), ord(sys.stdin.read(1))
+            if next1 == 91: # The second character in the arrow key sequence
+                if next2 == 65:
+                    return 'up'
+                elif next2 == 66:
+                    return 'down'
+                elif next2 == 67:
+                    return 'right'
+                elif next2 == 68:
+                    return 'left'
+        elif char == 32:
+            return 'space'
+        elif char == 10 or char == 13:
+            return 'enter'
+        elif char == 3: # CTRL-C
+            return 'exit'
+
+    def move_cursor_up(self):
+        if self.cursor_row > 0:
+            self.cursor_row -= 1
+
+    def move_cursor_down(self):
+        if self.cursor_row < self.height - 1:
+            self.cursor_row += 1
+
+    def move_cursor_right(self):
+        if self.cursor_col < self.width - 1:
+            self.cursor_col += 1
+
+    def move_cursor_left(self):
+        if self.cursor_col > 0:
+            self.cursor_col -= 1
+
+    def toggle_cell_state(self):
+        cell = self.grid[self.cursor_row][self.cursor_col]
+        if cell.state is 'dead':
+            cell.state = 'alive'
+        elif cell.state is 'alive':
+            cell.state = 'zombie'
+        else:
+            cell.state = 'dead'
+
+    def handle_keypress(self, keypress, mode):
+        if keypress is 'up':
+            self.move_cursor_up()
+        elif keypress is 'down':
+            self.move_cursor_down()
+        elif keypress is 'right':
+            self.move_cursor_right()
+        elif keypress is 'left':
+            self.move_cursor_left()
+        elif keypress is 'space':
+            self.toggle_cell_state()
+        elif keypress is 'enter':
+            tty.tcsetattr(sys.stdin, tty.TCSAFLUSH, mode)
+            return False
+        elif keypress is 'exit':
+            tty.tcsetattr(sys.stdin, tty.TCSAFLUSH, mode)
+            raise KeyboardInterrupt
+        return True
 
     def populate_board(self):
         mode = tty.tcgetattr(sys.stdin)
         tty.setraw(sys.stdin)
-        cursor_row = 0
-        cursor_col = 0
+        accepting_input = True
         borders = self.get_borders()
 
         instructions = ('Navigate the board with the arrow keys.\n\r'
@@ -84,48 +148,20 @@ class Board:
                         f'{self.BLACK_BACKGROUND}{self.GREEN_TEXT}zombie{self.TEXT_RESET} states.\n\r'
                         'Press Enter to begin the game once you are done selecting your starting cells.\n\n\r')
 
-        while True: # continue taking input
+        while accepting_input:
             rendered_board = f'{instructions}{borders["top"]}{self.BLACK_BACKGROUND}'
             for row in self.grid:
                 rendered_board += borders['left']
                 for cell in row:
-                    if cell.row is cursor_row and cell.col is cursor_col:
+                    if cell.row is self.cursor_row and cell.col is self.cursor_col:
                         rendered_board += self.CURSOR[cell.state]
                     else:
                         rendered_state = cell.next_state or cell.state
                         rendered_board += self.RENDERING_STRINGS[rendered_state]
                 rendered_board += f'{borders["right"]}\n\r'
 
-            os.system('cls' if os.name == 'nt' else 'clear')
+            print(chr(27) + '[2J' + chr(27) + '[0;0H') # clears terminal
             print(rendered_board + self.TEXT_RESET + borders['bottom'])
 
-            char = ord(sys.stdin.read(1)) # listen for arrow keys and get char code
-            if char == 27:
-                next1, next2 = ord(sys.stdin.read(1)), ord(sys.stdin.read(1))
-                if next1 == 91:
-                    if next2 == 65: # Up
-                        if cursor_row > 0:
-                            cursor_row -= 1
-                    elif next2 == 66: # Down
-                        if cursor_row < self.height - 1:
-                            cursor_row += 1
-                    elif next2 == 67: # Right
-                        if cursor_col < self.width - 1:
-                            cursor_col += 1
-                    elif next2 == 68: # Left
-                        if cursor_col > 0:
-                            cursor_col -= 1
-            elif char == 32: # Space
-                cell = self.grid[cursor_row][cursor_col]
-                if cell.state is 'dead':
-                    cell.state = 'alive'
-                elif cell.state is 'alive':
-                    cell.state = 'zombie'
-                else:
-                    cell.state = 'dead'
-            elif char == 10 or char == 13: # Enter
-                tty.tcsetattr(sys.stdin, tty.TCSAFLUSH, mode)
-                return
-            elif char == 3: # CTRL-C
-                tty.tcsetattr(sys.stdin, tty.TCSAFLUSH, mode)
-                raise KeyboardInterrupt
+            keypress = self.process_keyboard_input()
+            accepting_input = self.handle_keypress(keypress, mode)
